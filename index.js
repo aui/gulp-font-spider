d71e7f1980175d7ce371614dca466d153bd6257b
糖饼 <1987.tangbin@gmail.com>
2016-04-09 19:41:24 +0800

    支持显示日志

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 var FontSpider = require('font-spider');
 var through = require('through2');
+var gutil = require('gutil');
+var util = require('util');
 
 var createStream = function(options) {
     return through.obj(function(file, enc, callback) {
@@ -10,9 +12,15 @@ var createStream = function(options) {
         if (file.isBuffer && file.isBuffer()) {
             fontSpider = new FontSpider(file.path, options);
 
-            fontSpider.then(function() {
+            fontSpider.then(function(webFonts) {
+                var json = JSON.stringify(webFonts, null, 4);
+                json = JSON.parse(json);
+                gutil.log(util.inspect(json, {
+                    colors: true,
+                    depth: null
+                }));
                 callback(null, file);
-            });
+            }, callback);
 
         } else {
             callback(null, file);
